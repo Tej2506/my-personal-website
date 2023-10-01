b2c804f3e7f1963ef1a075a276f230d883185c71
Basavanna <basavannatej25@gmail.com>
2023-10-01 20:25:09 +1030

    added toggleProjectDetails function in index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function showContactPopup() {
     alert("Contact Information:\nName: Basavanna C D\nPhone: +61 423519844\nEmail: basavannatej@gmail.com");
 }
+
+function toggleProjectDetails(projectId) {
+    var project = document.getElementById(projectId);
+    if (project.style.display === "none" || project.style.display === "") {
+        project.style.display = "block";
+    } else {
+        project.style.display = "none";
+    }
+}
